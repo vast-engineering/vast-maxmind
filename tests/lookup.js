2b6becd79c82f48e66c03ba80f65448bce9d9f0d
Tommy Messbauer <totally.bro@gmail.com>
2012-05-17 15:34:14 -0500

    removed extra junk from test

--- tests/lookup.js
+++ tests/lookup.js
@@ -1,18 +1,8 @@
 var VastMaxmind = require('../build/Release/vastmaxmind').VastMaxmind,
 	vmm = new VastMaxmind('/usr/local/share/GeoIP/GeoIPCity.dat'),
-	addr = "8.8.8.8"; // 1029177344;
-	//addr = 1029177344;
-
-debugger;
+	addr = "8.8.8.8";
 
 var foo = vmm.location(addr, function(data) {
 	console.log(JSON.stringify(data) );
 	process.exit();
 });
-
-
-// var bad = new VastMaxmind('ksadfhjkdh');
-// var poo = vmm.location(addr, function(data) {
-// 	console.log(JSON.stringify(data) );
-// 	process.exit();
-// });
